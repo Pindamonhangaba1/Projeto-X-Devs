d7d26bf7805d4d8c5ed0c19384d22735d6057617
Pindamonhangaba1 <122842213+Pindamonhangaba1@users.noreply.github.com>
2024-07-26 14:28:39 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,37 @@
+const btnAvancar = document.getElementById("btn-avancar");
+const btnVoltar = document.getElementById("btn-voltar");
+let cartaoAtual = 0;
+const cartoes = document.querySelectorAll(".cartao");
+
+btnAvancar.addEventListener("click", function () {
+   const ehUltimoCartao = cartaoAtual === cartoes.length -1;
+   if (ehUltimoCartao === cartoes.length - 1) return;
+
+   esconderCartaoSelecionado();
+
+   cartaoAtual++;
+   mostrarCartao();
+});
+
+//- BOTÃO PARA VOLTAR A PARTIR DAQUI
+
+btnVoltar.addEventListener("click", function () {
+   const ehPrimeiroCartao = cartaoAtual === 0
+   if (ehPrimeiroCartao) return;
+
+   const cartaoSelecionado = document.querySelector(".selecionado");
+   cartaoSelecionado.classList.remove("selecionado");
+
+   cartaoAtual--;
+   mostrarCartao();
+
+});
+
+function mostrarCartao() {
+   cartoes[cartaoAtual].classList.add("selecionado");
+}
+
+function esconderCartaoSelecionado() {
+   const cartaoSelecionado = document.querySelector(".selecionado");
+   cartaoSelecionado.classList.remove("selecionado");
+}
